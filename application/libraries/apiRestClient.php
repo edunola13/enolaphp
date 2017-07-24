5335a3d00d748a32fd6c0578255be744edf474d4
edunola13 <edunola13@hotmail.com>
2017-07-24 13:10:28 -0300

    Detalles

--- application/libraries/apiRestClient.php
+++ application/libraries/apiRestClient.php
@@ -159,7 +159,7 @@ class RestClient {
     }
 }
 
-class RestClientException extends Exception {
+class RestClientException extends \Exception {
     public function __construct($message, $code = 500, Exception $previous = null) {
         // Default code 500
         parent::__construct($message, $code, $previous);
