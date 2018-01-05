80fa5c08bf2378aecbcb8b612b8204ad06b21477
Eduardo Nola <edunola13@hotmail.com>
2018-01-04 23:13:27 -0300

    Nuevo Modulo para la creacion de shell scripts propios del framework para facilitar el desarrollo. seguir creciendo

--- framework/cron/cron.php
+++ framework/cron/cron.php
@@ -38,6 +38,107 @@ class CronCore{
         $this->cronRequest= new En_CronRequest($config);
         $this->cronResponse= new Response();
     }    
+    
+    /**
+     * Ejecuta el cron correspondiente en base a los parametros pasados por la linea de comandos
+     * Utilizado solo por el framework
+     * Este es la funcion principal y define el flujo que va a seguir la peticion: CronManagement, CronDesdeComandos, ShellScript
+     */
+    public function executeCronController(){
+        $class= $this->cronRequest->getParamAll(1);
+        //Analizo si llamo a los controladores del usuario o si llamo al manejador de tareas o shell script del framework
+        if($class == 'CronManagement'){
+            //Ejecuto el CronManagement
+            $this->executeCronManagement();
+        }
+        if($class != 'CronManagement'){
+            $method= "index";
+            //Si la diferencia es mayor a 2 entre ambos arreglos de parametros quiere decir que se indico el nombre del metodo
+            if(count($this->cronRequest->getAllParams()) - count($this->cronRequest->getParams()) > 2){
+                $method= $this->cronRequest->getParamAll(2);
+            }
+            //Ejecuto Cron de Usuario o Shell Script del Framework
+            if(strpos($class, 'Eno-') === 0){
+                //Ejecuto el cron
+                $this->executeShellScript(substr($class, 4), $method);
+            }else{
+                //Ejecuto el cron
+                $this->executeCron($class, $method);
+            }
+        }
+    }
+    /**
+     * Ejecuta el cron management del framework el cual analiza las tareas definidas en el archivo de configuracion
+     * y ejecuta las que corresponda.
+     */
+    public function executeCronManagement(){
+        $dateTime= new \DateTime();
+        $actualFrequency= array(
+            'i' => $dateTime->format('i'),
+            'H' => $dateTime->format('H'),
+            'd' => $dateTime->format('d'),
+            'm' => $dateTime->format('m'),
+            'w' => $dateTime->format('w')
+        );        
+        $definedCrons= $this->app->context->readConfigurationFile('cronJobs')['crons'];
+        $cronsToExeture= array();
+        foreach ($definedCrons as $cronEsp) {
+            $frecuenciaActiva= $this->activeFrequency($cronEsp['frequency'], $actualFrequency);
+            if($frecuenciaActiva){
+                $cronsToExeture[]= $cronEsp;
+            }
+        }
+        //Ejecuto las tareas en la que su frecuencia sea activa
+        foreach ($cronsToExeture as $cronEsp) {
+            $propertiesEsp= isset($cronEsp['properties']) ? $cronEsp['properties'] : NULL;
+            $this->executeCron($cronEsp['cronController'], $cronEsp['method'], $propertiesEsp);
+        }        
+    }
+    /**
+     * Ejecuta el Cron mediante el metodo indicado
+     * Este es utilizado por el metodo forward del CronController y de uso interno al modulo
+     * @param string $cron
+     * @param string $method
+     * @param array $propertiesEsp
+     */
+    public function executeCron($cron, $method= "index", $propertiesEsp= NULL){
+        $dir= PATHAPP . 'source/crons/' . $cron . '.php';
+        //Analiza si existe el archivo
+        if(file_exists($dir)){
+            $cronIns= $this->instanceClass($dir, $cron, $propertiesEsp);
+            //Analiza si existe el metodo indicado
+            if(method_exists($cronIns, $method)){
+                $cronIns->$method($this->cronRequest, $this->cronResponse);                
+            }else{
+                Error::general_error('Cron Controller Error', 'The Cron Controller ' . $cron . ' dont implement the method ' . $method . '()');
+            }
+        }else{
+            Error::general_error('Cron Controller Error', 'The Cron Controller ' . $cron . ' dont exist');
+        }
+    }
+    /**
+     * Ejecuta el Shell Script mediante el metodo indicado
+     * Este es de uso interno al modulo
+     * @param string $shell
+     * @param string $method
+     * @param array $propertiesEsp
+     */
+    public function executeShellScript($shell, $method= "index", $propertiesEsp= NULL){
+        $dir= PATHFRA . 'src/shellscripts/' . $shell . '.php';
+        //Analiza si existe el archivo
+        if(file_exists($dir)){
+            $shellIns= $this->instanceClass($dir, $shell, $propertiesEsp);
+            //Analiza si existe el metodo indicado
+            if(method_exists($shellIns, $method)){
+                $shellIns->$method($this->cronRequest, $this->cronResponse);                
+            }else{
+                Error::general_error('Shell Script Error', 'The Shell Script ' . $shell . ' dont implement the method ' . $method . '()');
+            }
+        }else{
+            Error::general_error('Shell Script Error', 'The Shell Script ' . $shell . ' dont exist');
+        }
+    }
+    
     /**
      * Analiza los parametros y devuelvo los parametros ordenados para su posterior uso
      * El primer parametros es el index.php, el segundo es el nombre (clase) del cron y despues puede ser el metodo a ejecutar
@@ -94,77 +195,20 @@ class CronCore{
         return $condition;
     }
     /**
-     * Ejecuta el cron correspondiente en base a los parametros pasados por la linea de comandos
-     * Utilizado solo por el framework
+     * En base a una direccion y un nombre instancia la clase correspondiente e injecta las propiedades correspondientes
+     * @param string $dir
+     * @param string $name
+     * @param array $propertiesEsp
+     * @return \Enola\Cron\class
      */
-    public function executeCronController(){
-        $cron= $this->cronRequest->getParamAll(1);
-        //Analizo si llamo a los controladores del usuario o si llamo al manejador de tareas del framework
-        if($cron != 'CronManagement'){
-            $method= "index";
-            //Si la diferencia es mayor a 2 entre ambos arreglos de parametros quiere decir que se indico el nombre del metodo
-            if(count($this->cronRequest->getAllParams()) - count($this->cronRequest->getParams()) > 2){
-                $method= $this->cronRequest->getParamAll(2);
-            }
-            //Ejecuto el cron
-            $this->executeCron($cron, $method);
-        }else{
-            //Ejecuto el CronManagement
-            $this->executeCronManagement();
+    protected function instanceClass($dir, $name, $propertiesEsp= NULL){
+        require_once $dir;
+        $dir= explode("/", $name);
+        $class= $dir[count($dir) - 1];
+        $instance= new $class();
+        if($propertiesEsp != NULL){
+            $this->app->dependenciesEngine->injectProperties($instance, $propertiesEsp);
         }
-    }
-    /**
-     * Ejecuta el cron management del framework el cual analiza las tareas definidas en el archivo de configuracion
-     * y ejecuta las que corresponda.
-     */
-    public function executeCronManagement(){
-        $dateTime= new \DateTime();
-        $actualFrequency= array(
-            'i' => $dateTime->format('i'),
-            'H' => $dateTime->format('H'),
-            'd' => $dateTime->format('d'),
-            'm' => $dateTime->format('m'),
-            'w' => $dateTime->format('w')
-        );        
-        $definedCrons= $this->app->context->readConfigurationFile('cronJobs')['crons'];
-        $cronsToExeture= array();
-        foreach ($definedCrons as $cronEsp) {
-            $frecuenciaActiva= $this->activeFrequency($cronEsp['frequency'], $actualFrequency);
-            if($frecuenciaActiva){
-                $cronsToExeture[]= $cronEsp;
-            }
-        }
-        //Ejecuto las tareas en la que su frecuencia sea activa
-        foreach ($cronsToExeture as $cronEsp) {
-            $propertiesEsp= isset($cronEsp['properties']) ? $cronEsp['properties'] : NULL;
-            $this->executeCron($cronEsp['cronController'], $cronEsp['method'], $propertiesEsp);
-        }        
-    }
-    /**
-     * Ejecuta el Cron mediante el metodo indicado
-     * Este es utilizado por el metodo forward del CronController y de uso interno al modulo
-     * @param string $cron
-     * @param string $method
-     */
-    public function executeCron($cron, $method= "index", $propertiesEsp= NULL){
-        $dir= PATHAPP . 'source/crons/' . $cron . '.php';
-        //Analiza si existe el archivo
-        if(file_exists($dir)){
-            require_once $dir;
-            $dir= explode("/", $cron);
-            $class= $dir[count($dir) - 1];
-            $cronIns= new $class();
-            if($propertiesEsp != NULL){
-                $this->app->dependenciesEngine->injectProperties($cron, $propertiesEsp);
-            }
-            //Analiza si existe el metodo indicado
-            if(method_exists($cronIns, $method)){
-                $cronIns->$method($this->cronRequest, $this->cronResponse);                
-            }else{
-                Error::general_error('Cron Controller Error', 'The Cron Controller ' . $cron . ' dont implement the method ' . $method . '()');
-            }
-        }else{
-            Error::general_error('Cron Controller Error', 'The Cron Controller ' . $cron . ' dont exist');
-        }
+        return $instance;
     }
 }
